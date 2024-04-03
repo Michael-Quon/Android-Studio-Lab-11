03546d9aadf504877d23d97a871deb0a18273104
Michael-Quon <michaelquon08@gmail.com>
2024-04-03 15:08:28 -0400

    Deleted module level .gitignore and edited project level.

--- MichaelLab11/src/main/java/michael/quon/n01565129/mq/QuonActivity11.java
+++ MichaelLab11/src/main/java/michael/quon/n01565129/mq/QuonActivity11.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class QuonActivity11 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
